c4b5482e03f74708360ee23272434ec7f3c8c154
tenmaster <tenlion@hotmail.com>
2014-05-23 16:43:24 -0300

    Report Box setProperties

--- FYP/FYPReportBoxViewController.h
+++ FYP/FYPReportBoxViewController.h
@@ -7,6 +7,7 @@
 //
 
 #import <Foundation/Foundation.h>
+#import "UIImageView+AFNetworking.h"
 
 @interface FYPReportBoxViewController : NSObject
 
--- FYP/FYPReportBoxViewController.m
+++ FYP/FYPReportBoxViewController.m
@@ -21,10 +21,25 @@
     place = [infoFeed objectForKey: @"place"];
     date = [infoFeed objectForKey: @"date"];
     name = [infoFeed objectForKey: @"name"];
-    image = [infoFeed objectForKey:@"image"];
     
+
+    [image setImageWithURL:[infoFeed objectForKey:@"imageURL"]
+        placeholderImage:[UIImage imageNamed:@"tracks"]];
+
+    if ([[infoFeed objectForKey:@"gender"] isEqualToString:@"male"])
+        gender.image = [UIImage imageNamed:@"male"];
+    else if ([[infoFeed objectForKey:@"gender"] isEqualToString:@"female"])
+        gender.image = [UIImage imageNamed:@"femaleicon"];
+    else
+        gender.image = nil;
+    
+    if ([[infoFeed objectForKey:@"sightlost"] isEqualToString:@"sight"])
+        gender.image = [UIImage imageNamed:@"sight_light"];
+    else if ([[infoFeed objectForKey:@"searchlost"] isEqualToString:@"lost"])
+        gender.image = [UIImage imageNamed:@"search_light"];
+    else
+        gender.image = nil;
     
 }
-
 
 @end
